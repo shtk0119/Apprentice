f1eb5c7398fd2d3f34672da3a73b525b10ba2315
shtk0119 <shtk0119@icloud.com>
2024-02-22 12:47:38 +0900

    step1 create

--- Quest/Laravel/Conduit/routes/web.php
+++ Quest/Laravel/Conduit/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/article/{article}', function (string $id) {
+    return view('article');
+});
+
+Route::get('/create', function () {
+    return view('create');
+});
+
+Route::get('/edit/{article}', function (string $id) {
+    return view('edit');
 });
